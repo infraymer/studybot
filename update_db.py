56a482b64f791f8cee9eb3040307730d96920711
infraymer <dok3pl@yandex.ru>
2018-11-25 21:22:35 +0500

    add server for update task in database

---
+++ update_db.py
@@ -0,0 +1,48 @@
+import json
+
+from flask import Flask, request, flash, redirect
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.bot
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello world!'
+
+
+@app.route('/tasks', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+        with open('data.json', encoding='utf-8') as f:
+            data = json.load(f)
+            db.tasks.remove({})
+            db.tasks.insert(data)
+
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+
+if __name__ == '__main__':
+    app.run()
